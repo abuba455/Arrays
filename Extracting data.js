eabfdf921726e56d6b02179974003d364c586964
RamzyCodes101 <abubakarsidique796@gmail.com>
2025-10-06 16:41:55 +0000

    map() method to extract brand name

---
+++ Extracting data.js
@@ -0,0 +1,8 @@
+let cars = [
+    {brand: 'Suzuki', price: 30000},
+    {brand: 'Toyota', price: 40000},
+    {brand: 'ferrari', pricce: 18000}
+]
+
+let brands = cars.map(car => car.price)
+console.log(brands)
